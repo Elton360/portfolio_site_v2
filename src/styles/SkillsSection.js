2786f77e45b83ea8a8d88054622eee588116dff0
Elton360 <elton.lucien1@gmail.com>
2025-02-08 17:21:38 -0500

    Fix grid issue on Firefox

--- src/styles/SkillsSection.js
+++ src/styles/SkillsSection.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const SkillsContainer = styled.div`
   max-width: 100%;
+  width: 160rem;
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
   grid-gap: 2rem;
